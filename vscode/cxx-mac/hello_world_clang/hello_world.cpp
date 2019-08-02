5b761fc0cb29bc7c4ad832021fe6b1256d70466e
vincent <shix_112@126.com>
2019-08-03 05:37:15 +0800

    区分 vscode mac/windows 下的配置文件

---
+++ vscode/cxx-mac/hello_world_clang/hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int age = 0;
+    string c = "Init a string value";
+    std::cout << "hello, world"  << std::endl;
+    age += 1;
+    std::cout << "age is " << age << std::endl;
+    return 0;
+}
---
+++ vscode/python-mac/hello_world_python2.7/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# *-* coding:utf-8 *-*
+
+print "hello"
+
+def test():
+    return True if 1 == 2 else False
+
+print test()
